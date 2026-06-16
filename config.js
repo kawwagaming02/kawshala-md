da6c82642707466465484c14f875a6b964696e25
kawwagaming02 <kawwagaming45@gmail.com>
2026-06-17 00:56:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "QIckECaL#EZphi15R21Mizq0oMxKBVkIV_8bbPsZk5_h6_AG2tS8",
+SESSION_ID: process.env.SESSION_ID || "4YchyZgS#5uBz0Xa6oG4oVi1Qthlt6oyDWiK1EhCiXYtYFelHBRM",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/kawwagaming02/kawshala-md/blob/main/images/KAWSHALA-MD%20(1).jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 KAWSHALA-MD Is Alive Now😍*",
 BOT_OWNER: '94710189823',  // Replace with the owner's phone number
